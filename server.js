6cc409b911491f43114feacb86a8e300438e8c94
Stan Cosmin <stancosminioan@gmail.com>
2024-11-19 23:35:28 +0200

    [WIP] - added all the functionality

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("TODO APP");
+app.use(logger);
+
+app.use(credentials);
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+
+app.use(verifyJWT);
+app.use("/employees", require("./routes/api/employees"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
-const tasksRouter = require("./routes/tasks");
+app.use(errorHandler);
 
-app.use("/tasks", tasksRouter);
-
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
