b5392044b45b848c20f8501ae1917258a228b365
Stan Cosmin <stancosminioan@gmail.com>
2024-11-21 13:52:19 +0200

    [WIP]- fixed some bugs

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -4,8 +4,7 @@ const usersDB = {
     this.users = data;
   },
 };
-const fsPromises = require("fs").promises;
-const path = require("path");
+const roles = require("../config/roles_list");
 const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
@@ -22,15 +21,10 @@ const handleNewUser = async (req, res) => {
 
     const newUser = {
       username: user,
-      roles: { User: 2001 },
+      roles: { User: roles.User },
       password: hashedPwd,
     };
     usersDB.setUsers([...usersDB.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
-    console.log(usersDB.users);
     res.status(201).json({ success: `New user ${user} created!` });
   } catch (err) {
     res.status(500).json({ message: err.message });
