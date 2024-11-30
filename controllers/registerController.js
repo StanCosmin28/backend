b3b5ba82c5fa3335feab736cd99cd2d884240fe2
Stan Cosmin <stancosminioan@gmail.com>
2024-11-30 20:01:50 +0200

    [WIP]-controllers use DB

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,9 +1,3 @@
-// const usersDB = {
-//   users: require("../model/users.json"),
-//   setUsers: function (data) {
-//     this.users = data;
-//   },
-// };
 const User = require("../model/User");
 const roles = require("../config/roles_list");
 const bcrypt = require("bcrypt");
@@ -15,23 +9,18 @@ const handleNewUser = async (req, res) => {
       .status(400)
       .json({ message: "Username and password are required." });
 
-  // const duplicate = usersDB.users.find((person) => person.username === user);
   //find the duplicate using the username in the database with the one we recieved from the client
-  const duplicate = await User.findOne({
-    username: user,
-  }).exec();
+  const duplicate = await User.findOne({ username: user }).exec();
 
   if (duplicate) return res.sendStatus(409); //Conflict
   try {
     const hashedPwd = await bcrypt.hash(pwd, 10);
 
-    const newUser = {
+    const result = User.create({
       username: user,
-      roles: { User: roles.User },
       password: hashedPwd,
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    res.status(201).json({ success: `New user ${user} created!` });
+    });
+    res.status(201).json({ success: `New user ${result.user} created!` });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
